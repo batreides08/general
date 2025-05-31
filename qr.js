7d8f5b5cc992812e780b04f44dda3aa95a7746e8
batreides08 <bquiroscamacho@byupathway.edu>
2025-05-30 22:29:10 -0600

    Initial commit

---
+++ qr.js
@@ -0,0 +1,21 @@
+// data.js
+const participants = {
+  juan123: {
+    name: "Juan Pérez",
+    age: 16,
+    phone: "8888-5555",
+    emergency: "Mamá - 8888-7777",
+    photo: "images/hs.webp",
+    barrio: "La Rita",
+    
+  },
+  maria456: {
+    name: "María González",
+    age: 17,
+    phone: "8888-4444",
+    emergency: "Papá - 8888-3333",
+    photo: "images/hsg.webp",
+    barrio: "La Rita",
+  },
+  // add more participants here
+};
