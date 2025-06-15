e533d6f959eac8d2f03a9a1bf4a12312571f9525
batreides08 <bquiroscamacho@byupathway.edu>
2025-06-15 16:32:36 -0600

    111

--- qr.js
+++ qr.js
@@ -4,8 +4,6 @@ const participants = {
     name: "Juan Pérez",
     age: 16,
     phone: "8888-5555",
-    emergency: "Mamá - 8888-7777",
-    photo: "images/hs.webp",
     barrio: "La Rita",
     
   },
@@ -13,8 +11,6 @@ const participants = {
     name: "María González",
     age: 17,
     phone: "8888-4444",
-    emergency: "Papá - 8888-3333",
-    photo: "images/hsg.webp",
     barrio: "La Rita",
   },
   // add more participants here
