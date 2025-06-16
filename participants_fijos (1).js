f10d6997081abeec3f9b970f7368625538e14af8
batreides08 <bquiroscamacho@byupathway.edu>
2025-06-15 22:18:43 -0600

    999999

--- participants_fijos (1).js
+++ participants_fijos (1).js
@@ -1,20 +1,5 @@
-// data.js
 const participants = {
-  juan123: {
-    name: "Juan Pérez",
-    age: 16,
-    phone: "8888-5555",
-    barrio: "La Rita",
-    
-  },
-  maria456: {
-    name: "María González",
-    age: 17,
-    phone: "8888-4444",
-    barrio: "La Rita",
-  },
-  // add more participants here
-   keysha15: {
+  keysha15: {
     name: "Keyshany picado chamorro",
     age: 15,
     phone: "61237249",
@@ -260,7 +245,4 @@ const participants = {
     phone: "nan",
     barrio: "Puerto Viejo"
   },
-
 };
-
-
